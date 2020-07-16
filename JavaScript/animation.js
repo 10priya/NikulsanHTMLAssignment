bfe735295f9bbe4479fd06e7b97ce3fe8268941b
Priya Jain <priyajain10may@gmail.com>
2020-07-16 20:41:20 +0530

    Angular Assignmnet

---
+++ JavaScript/animation.js
@@ -0,0 +1,158 @@
+
+$(window).scroll(function() {
+    $('#animate').each(function(){
+        if ($(this).is(":in-viewport")) {
+            const element1 = document.querySelector('#info1')
+            element1.classList.add('animated', 'fadeInUp')
+        } else {
+            const element1 = document.querySelector('#info1')
+            element1.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+    
+$(window).scroll(function() {
+    $('#animate1').each(function(){
+        if ($(this).is(":in-viewport")) {
+            const element2 = document.querySelector('#info2')
+            element2.classList.add('animated', 'fadeInUp')
+        } else {
+            const element2 = document.querySelector('#info2')
+            element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+    
+    
+$(window).scroll(function() {
+    $('#animate2').each(function(){
+        if ($(this).is(":in-viewport")) {
+        const element1 = document.querySelector('#info3')
+        element1.classList.add('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info4')
+        element2.classList.add('animated', 'fadeInUp')
+        } else {
+        const element1 = document.querySelector('#info3')
+        element1.classList.remove('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info4')
+        element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+
+$(window).scroll(function() {
+    $('#animate3').each(function(){
+        if ($(this).is(":in-viewport")) {
+        const element1 = document.querySelector('#info5')
+        element1.classList.add('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info6')
+        element2.classList.add('animated', 'fadeInUp')
+        } else {
+        const element1 = document.querySelector('#info5')
+        element1.classList.remove('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info6')
+        element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+
+$(window).scroll(function() {
+    $('#animate4').each(function(){
+        if ($(this).is(":in-viewport")) {
+        const element1 = document.querySelector('#info7')
+        element1.classList.add('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info8')
+        element2.classList.add('animated', 'fadeInUp')
+        } else {
+        const element1 = document.querySelector('#info7')
+        element1.classList.remove('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info8')
+        element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+
+$(window).scroll(function() {
+    $('#animate5').each(function(){
+        if ($(this).is(":in-viewport")) {
+        const element1 = document.querySelector('#info9')
+        element1.classList.add('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info10')
+        element2.classList.add('animated', 'fadeInUp')
+        } else {
+        const element1 = document.querySelector('#info9')
+        element1.classList.remove('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info10')
+        element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+
+
+$(window).scroll(function() {
+    $('#animate6').each(function(){
+        if ($(this).is(":in-viewport")) {
+        const element1 = document.querySelector('#info11')
+        element1.classList.add('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info12')
+        element2.classList.add('animated', 'fadeInUp')
+        } else {
+        const element1 = document.querySelector('#info11')
+        element1.classList.remove('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info12')
+        element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+
+$(window).scroll(function() {
+    $('#animate7').each(function(){
+        if ($(this).is(":in-viewport")) {
+        const element1 = document.querySelector('#info13')
+        element1.classList.add('animated', 'fadeInUp')
+        } else {
+        const element2 = document.querySelector('#info13')
+        element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+
+$(window).scroll(function() {
+    $('#animate8').each(function(){
+        if ($(this).is(":in-viewport")) {
+        const element1 = document.querySelector('#info14')
+        element1.classList.add('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info15')
+        element2.classList.add('animated', 'fadeInUp')
+        const element3 = document.querySelector('#info16')
+        element3.classList.add('animated', 'fadeInUp')
+        const element4 = document.querySelector('#info17')
+        element4.classList.add('animated', 'fadeInUp')
+        } else {
+        const element1 = document.querySelector('#info14')
+        element1.classList.remove('animated', 'fadeInUp')
+        const element2 = document.querySelector('#info15')
+        element2.classList.remove('animated', 'fadeInUp')
+        const element3 = document.querySelector('#info16')
+        element3.classList.remove('animated', 'fadeInUp')
+        const element4 = document.querySelector('#info17')
+        element4.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
+
+$(window).scroll(function() {
+    $('#animate9').each(function(){
+        if ($(this).is(":in-viewport")) {
+            const element1 = document.querySelector('#info18')
+            element1.classList.add('animated', 'fadeInUp')
+            const element2 = document.querySelector('#info19')
+            element2.classList.add('animated', 'fadeInUp')
+        } else {
+            const element1 = document.querySelector('#info18')
+            element1.classList.remove('animated', 'fadeInUp')
+            const element2 = document.querySelector('#info19')
+            element2.classList.remove('animated', 'fadeInUp')
+        }
+    })
+});
